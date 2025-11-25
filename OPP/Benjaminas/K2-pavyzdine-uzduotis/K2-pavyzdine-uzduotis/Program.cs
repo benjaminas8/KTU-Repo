62fbdf220bdabeb734ca58a90a0db0c1c28c7ed7
benjaminas8 <benasceik@gmail.com>
2025-11-25 21:55:52 +0200

    started k2

---
+++ OPP/Benjaminas/K2-pavyzdine-uzduotis/K2-pavyzdine-uzduotis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2_pavyzdine_uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] skyrikliai = { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r', '\t' };
+            Console.WriteLine("Įveskite tekstą:");
+            string tekstas = Console.ReadLine();
+            Console.WriteLine("{0} skirtingu balsiu", SkirtBalsiuSkaicius(tekstas));
+        }
+        static int SkirtBalsiuSkaicius(string e)
+        {
+            int kiek = 0;
+            e = e.ToLower();
+            char[] balses = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            bool[] rasta = new bool[balses.Length];
+
+            for (int i = 0; i < balses.Length; i++)
+            {
+                if (!rasta[i] && e.Contains(balses[i]))
+                {
+                    rasta[i] = true;
+                    kiek++;
+                }
+            }
+            return kiek;
+        }
+    }
+}
